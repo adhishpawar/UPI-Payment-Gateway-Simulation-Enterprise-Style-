abd962b54ddbf4bb2aa20d21c55226bded4ea2a4
adhish <adhishpawar@gmail.com>
2025-09-15 15:20:28 +0530

    pspService init

---
+++ psp-service/src/main/java/com/upi/psp/PspServiceApplication.java
@@ -0,0 +1,13 @@
+package com.upi.psp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PspServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PspServiceApplication.class, args);
+	}
+
+}
